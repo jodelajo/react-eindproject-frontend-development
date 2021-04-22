94f92657af70e58c7c83b11687462a999bd9b5b2
jodelajo <jodelajo@gmail.com>
2021-04-22 11:54:55 +0200

    cleanup

--- src/pages/friesland/Friesland.js
+++ src/pages/friesland/Friesland.js
@@ -1,11 +1,21 @@
-import React, { useContext } from 'react';
-import { LocationContext } from "../../context/LocationContext";
+import React, {useContext} from 'react';
+import {LocationContext} from "../../context/LocationContext";
 import Counter from "../../components/counter/Counter";
 
 function Friesland() {
-    const {locationsWadden, locationsZuidOost, locationsNoordWest, locationsNoordOost, locationsZuidWest} = useContext(LocationContext)
+    const {
+        locationsWadden,
+        locationsZuidOost,
+        locationsNoordWest,
+        locationsNoordOost,
+        locationsZuidWest
+    } = useContext(LocationContext)
 
-    const frieslandLocations = locationsWadden.concat(locationsZuidOost).concat(locationsNoordWest).concat(locationsNoordOost).concat(locationsZuidWest)
+    const frieslandLocations = locationsWadden
+        .concat(locationsZuidOost)
+        .concat(locationsNoordWest)
+        .concat(locationsNoordOost)
+        .concat(locationsZuidWest)
 
     console.log('friesland', frieslandLocations)
 
@@ -13,18 +23,18 @@ function Friesland() {
     return <div>
         <h1>TOP 5 beste Plakjes</h1>
         <ul>
-            {frieslandLocations && frieslandLocations.sort((a,b)=>
-               b.totalPoints - a.totalPoints).slice(0,5)
+            {frieslandLocations && frieslandLocations.sort((a, b) =>
+                b.totalPoints - a.totalPoints).slice(0, 5)
                 .map((location) => {
-                return <Counter
-                    key={location.locationID}
-                    location={location}
-                    clouds={location.locationClouds}
-                    wind={location.locationWind}
-                    temp={location.locationTemp}
-                    totalPoints={location.totalPoints}
-                />
-            })}
+                    return <Counter
+                        key={location.locationID}
+                        location={location}
+                        clouds={location.locationClouds}
+                        wind={location.locationWind}
+                        temp={location.locationTemp}
+                        totalPoints={location.totalPoints}
+                    />
+                })}
         </ul>
     </div>
 }
