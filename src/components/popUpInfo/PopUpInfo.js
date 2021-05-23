8f3b9a04d51034f5b1ea1ceb45e564ba3a6776dd
jodelajo <jodelajo@gmail.com>
2021-05-23 13:44:09 +0200

    video

--- src/components/popUpInfo/PopUpInfo.js
+++ src/components/popUpInfo/PopUpInfo.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import  './PopUpInfo.css'
 import {RiStarLine} from "react-icons/all";
+import firstInfo from '../../assets/gifs/first-info.mp4'
 
 function PopUpInfo(){
     return <div className="popup-wrapper">
-        <h1>
+        <h3>
             Klik op een regio of op <RiStarLine/>
-        </h1>
-        <img className="first-gif" src={"https://media.giphy.com/media/RVmErCMMvnlpkzA5iC/giphy.gif"} alt="first gif"/>
-
+        </h3>
+        {/*<img className="first-gif" src={"https://media.giphy.com/media/RVmErCMMvnlpkzA5iC/giphy.gif"} alt="first gif"/>*/}
+        {/*<img src={firstInfo} alt="frist gif" />*/}
+        <video  className="video" controls autoPlay>
+            <source src={firstInfo} type="video/mp4"/>
+        </video>
     </div>
 }
 export default PopUpInfo
