53180606985859dbe3b54732a139e5e174727638
jodelajo <jodelajo@gmail.com>
2021-05-23 17:18:07 +0200

    slick slider

--- src/components/popUpInfo/PopUpInfo.js
+++ src/components/popUpInfo/PopUpInfo.js
@@ -1,18 +1,48 @@
 import React from 'react'
 import  './PopUpInfo.css'
 import {RiStarLine} from "react-icons/all";
-import firstInfo from '../../assets/gifs/first-info.mp4'
+import firstInfo from '../../assets/gifs/first-info.gif'
+import infoTwo from '../../assets/gifs/info-two.gif'
+import Slider from 'react-slick'
+
+import "./slick-theme.css";
+import "./slick.css";
+
+
 
 function PopUpInfo(){
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // adaptiveHeight: true
+    };
+
     return <div className="popup-wrapper">
         <h3>
             Klik op een regio of op <RiStarLine/>
         </h3>
         {/*<img className="first-gif" src={"https://media.giphy.com/media/RVmErCMMvnlpkzA5iC/giphy.gif"} alt="first gif"/>*/}
-        {/*<img src={firstInfo} alt="frist gif" />*/}
-        <video  className="video" controls autoPlay>
-            <source src={firstInfo} type="video/mp4"/>
-        </video>
+
+
+
+        <Slider {...settings}>
+            <div>
+                <img src={firstInfo} alt="frist gif" className="first-gif"/>
+            </div>
+            <div>
+                <img src={infoTwo} alt="frist gif" className="first-gif"/>
+
+            </div>
+        </Slider>
+
+
+
+
+
     </div>
 }
 export default PopUpInfo
