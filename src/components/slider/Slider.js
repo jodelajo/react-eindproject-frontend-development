62b7aca11f2f73a49863cb4cef59470e9f637398
jodelajo <jodelajo@gmail.com>
2021-05-25 12:30:02 +0200

    uitleg gifs

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,26 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Slider.css'
-import infoTwo from "../../assets/gifs/info-two.gif";
-
-import uno from "../../assets/gifs/uno.gif"
-
+import uitleg2 from "../../assets/gifs/uitleg2.gif";
+import uitleg1 from "../../assets/gifs/uitleg1.gif";
+import uitleg3 from "../../assets/gifs/uitleg3.gif"
 import {RiArrowLeftLine, RiArrowRightLine} from "react-icons/ri";
 
-function Slider(  ) {
+function Slider() {
     const [currentSlide, setCurrentSlide] = useState(1)
-    // const { length } = slides
-
 
     const slides = [
 
         {
-            title: 'Klik op een uno',
-            image: `${uno}`
+            title: `Klik op een regio voor Top 5 locaties`,
+            image: `${uitleg1}`
         },
         {
-            title: 'Klik op een booster',
-            image: `${infoTwo}`
+            title: 'Klik op een Booster voor extra punten voor zon, temperatuur en/of wind.',
+            image: `${uitleg2}`
+        },
+        {
+            title: 'Maak een account aan of log in.',
+            image: `${uitleg3}`
         }
     ]
 
@@ -33,32 +34,44 @@ function Slider(  ) {
     }
 
     useEffect(() => {
-            setCurrentSlide(goToNext)
+        setCurrentSlide(goToNext)
     }, [])
 
     useEffect(() => {
         setCurrentSlide(goToPrevious)
     }, [])
 
-    if(!Array.isArray(slides) || slides.length <= 0){
+    if (!Array.isArray(slides) || slides.length <= 0) {
         return null;
     }
     console.log('currentSlide?', currentSlide)
 
     return <div className="slider">
-        <button type="button" className="slider-button" onClick={goToPrevious}><RiArrowLeftLine/></button>
-        {slides.map((slide, index) =>(
+        <button type="button"
+                className="slider-button"
+                onClick={goToPrevious}
+                disabled={currentSlide === 0}
+        >
+            <RiArrowLeftLine/>
+        </button>
+        {slides.map((slide, index) => (
 
-            <div  className={index === currentSlide ? "slide active" : "slide"} key={slide.title}>
+            <div className={index === currentSlide ? "slide active" : "slide"} key={slide.title}>
                 <div>
                     <h2>{slide.title}</h2>
                 </div>
-                {slide &&(
+                {slide && (
                     <img className="gif" src={slide.image} alt="slider"/>
                 )}
             </div>
         ))}
-        <button type="button" className="slider-button" onClick={goToNext}><RiArrowRightLine/></button>
+        <button
+            type="button"
+            className="slider-button"
+            onClick={goToNext}>
+            <RiArrowRightLine/>
+        </button>
     </div>
 }
+
 export default Slider
